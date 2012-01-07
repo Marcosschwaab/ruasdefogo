a0789d38a812fd93ff15603e67930cb18ddf2e1d
Marcos Schwaab <marcos.schwaab@gmail.com>
2012-01-07 11:24:21 -0800

    inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ruasdefogo::Application.config.secret_token = '3d18600f31344396d76ba5f7d4c79a25915517d3a565f376118c6e468aff868d24f56430f2ce3560fac66e00b3a674bc16de1699d9c24f497a9f67232d8b647e'
